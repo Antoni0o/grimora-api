7dbeb749fb604848d28b457e916535a94c45d62c
Antoni0o <antonio.dias8997@gmail.com>
2025-06-01 13:56:57 -0300

    dependency injection

--- src/app/systems/systems.module.ts
+++ src/app/systems/systems.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { SystemsService } from './application/systems.service';
 import { SystemsController } from './application/systems.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { SystemMongoSchema, SystemSchema } from './infraestructure/persistence/system.schema';
+import { SYSTEM_REPOSITORY } from './domain/constants/system.constants';
+import { SystemRepository } from './infraestructure/persistence/system.mongoose.repository';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: SystemMongoSchema.name, schema: SystemSchema }])],
   controllers: [SystemsController],
-  providers: [SystemsService],
+  providers: [
+    SystemsService,
+    {
+      provide: SYSTEM_REPOSITORY,
+      useClass: SystemRepository,
+    },
+  ],
 })
 export class SystemsModule {}
