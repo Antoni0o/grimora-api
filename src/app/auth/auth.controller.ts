85390a7975c8398fadc21f64bef5f53183346de2
Antoni0o <antonio.dias8997@gmail.com>
2025-06-02 22:55:52 -0300

    fix dto folder name

--- src/app/auth/auth.controller.ts
+++ src/app/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthRequestDto } from './dtos/auth-request.dto';
+import { AuthRequestDto } from './dto/auth-request.dto';
 
 @Controller('auth')
 export class AuthController {
