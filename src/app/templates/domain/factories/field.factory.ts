3502e44350d8d3cfe5d02ec93b6e7ed22e2dd052
Antoni0o <antonio.dias8997@gmail.com>
2025-06-12 23:55:59 -0300

    should create a group field of the correct type (GREEN)

--- src/app/templates/domain/factories/field.factory.ts
+++ src/app/templates/domain/factories/field.factory.ts
@@ -1,4 +1,5 @@
 import { Field } from "../entities/fields/field.entity";
+import { GroupField } from "../entities/fields/group-field.entity";
 import { FieldData } from "../interfaces/field.interface";
 import { fieldRegistry } from "../registries/field.registry";
 
@@ -8,6 +9,14 @@ export class FieldFactory {
 
         if (!fieldClass) throw new Error(`Field type ${data.type} is not registered.`);
 
-        return new fieldClass(data);
+        const fieldInstance = new fieldClass(data);
+
+        if (fieldInstance instanceof GroupField && (data.fields && data.fields.length > 0)) {
+            fieldInstance.fields = data.fields.map(childFieldData => {
+                return FieldFactory.create(childFieldData);
+            })
+        }
+
+        return fieldInstance;
     }
 }
